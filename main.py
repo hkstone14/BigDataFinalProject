934ea1be623e27e43b15ff148bd576e5b5d783ad
harikrushna <hp52@njit.edu>
2021-11-18 15:44:43 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import csv
+from textblob import TextBlob
+import re
+import emoji
+import nltk
+from pyspark.ml.classification import DecisionTreeClassifier
+
+
+nltk.download('words')
+words = set(nltk.corpus.words.words())
+
+infile = 'training.1600000.processed.noemoticon.csv'
+clean_tweet = ''
+
+training_data = pd.read_csv(infile, encoding="ISO-8859-1")
+y = training_data['0']
+
+labelIndexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(data)
+
+
+def cleaner(tweet):
+    tweet = re.sub("@[A-Za-z0-9]+", "", tweet)  # Remove @ sign
+    tweet = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", "", tweet)  # Remove http links
+    tweet = " ".join(tweet.split())
+    tweet = ''.join(c for c in tweet if c not in emoji.UNICODE_EMOJI)  # Remove Emojis
+    tweet = tweet.replace("#", "").replace("_", " ")  # Remove hashtag sign but keep the text
+    tweet = " ".join(w for w in nltk.wordpunct_tokenize(tweet) \
+                     if w.lower() in words or not w.isalpha())
+    return tweet
+
+
+with open(infile, 'r') as csvfile:
+    rows = csv.reader(csvfile)
+    for row in rows:
+        data = row[5]
+        clean_tweet = cleaner(data)
+        model = DecisionTreeClassifier()
+        model.fit(clean_tweet, y)
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
