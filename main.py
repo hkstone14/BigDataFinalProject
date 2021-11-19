be385ba15d159a230ddd1b8986214b5e5d53cac0
harikrushna <hp52@njit.edu>
2021-11-19 15:33:18 -0500

    Task : Data cleaning

--- main.py
+++ main.py
@@ -41,9 +41,3 @@ with open(infile, 'r') as csvfile:
 
 
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
