d7ea1fbaa45fbd31860c6279e73fc5355c92c694
harikrushna <hp52@njit.edu>
2021-12-10 18:09:22 -0500

    fix : minor fix

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 
 if __name__ == "__main__":
     HOST = 'localhost'  # The server's hostname or IP address
-    PORT = 5555  # The port used by the server
+    PORT = 9009  # The port used by the server
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
