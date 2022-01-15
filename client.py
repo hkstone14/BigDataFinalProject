87df827f77cb28dd44c7040fa7d1a8d9957090a0
harikrushna <hp52@njit.edu>
2022-01-14 20:56:30 -0500

    Fix : Minor fx

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import pickle
 
 if __name__ == "__main__":
     HOST = 'localhost'  # The server's hostname or IP address
-    PORT = 9009  # The port used by the server
+    PORT = 5555# The port used by the server
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
